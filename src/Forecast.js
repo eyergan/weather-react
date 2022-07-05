6a506261d95432f512ed027064ac02f75ab62e03
eyergan <eyergan1@gmail.com>
2022-07-04 21:07:38 -0400

    integrated api into forecast for 1 day call

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Forecast.css";
 
 export default function Forecast(props) {
   return (
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,26 +1,36 @@
-import React from "react";
-import Forecast from "./Forecast";
+import React, { useState } from "react";
 import axios from "axios";
+import "./WeatherForecast.css";
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  let apiKey = "719c79f57389bdae3a53f02f543b77e6";
-  let latitude = props.coordinates.lat;
-  let longitude = props.coordinates.lon;
-  let apiUrl = `
+  if (loaded) {
+    console.log(forecast);
+
+    return (
+      <div className="card week" id="forecast">
+        <h5 className="card-title week-title">Forecast</h5>
+        <ul className="list-group list-group-flush" id="forecastList">
+          <li className="list-group-item weather-forecast-temp">
+            <span className="forecast-list">{forecast[0].dt}</span>
+            <span className="forecast-list">{forecast[0].temp.max}</span>
+
+            <span className="forecast-list">{forecast[0].temp.min}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    let apiKey = "719c79f57389bdae3a53f02f543b77e6";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `
 https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="card week" id="forecast">
-      <h5 className="card-title week-title">Forecast</h5>
-      <ul className="list-group list-group-flush" id="forecastList">
-        <Forecast day="mon" icon="icon" temp="80" tempMin="0" />
-        <Forecast day="tues" icon="icon" temp="70" tempMin="0" />
-        <Forecast day="wed" icon="icon" temp="75" tempMin="0" />
-        <Forecast day="thurs" icon="icon" temp="80" tempMin="0" />
-        <Forecast day="fri" icon="icon" temp="72" tempMin="0" />
-      </ul>
-    </div>
-  );
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
